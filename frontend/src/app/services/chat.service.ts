5ec8338066101685052fa3302481529434b8b379
AdeulianSuke <>
2026-07-27 10:03:57 +0200

    Config environements and point to backend in render

--- frontend/src/app/services/chat.service.ts
+++ frontend/src/app/services/chat.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
-  private apiUrl = 'http://localhost:8000/chat';
+  private apiUrl = environment.apiUrl;
   private sessionId = this.generateSessionId();
 
   constructor(private http: HttpClient) {}
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://chatbot-3lw8.onrender.com/chat'
+};
